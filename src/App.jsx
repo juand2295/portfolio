b2623a5d108f0a140134bddd026b25827000ea0e
Juan David Perez <juandaperezvelez@hotmail.com>
2023-11-14 17:24:29 -0500

    more tags fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import { BrowserRouter } from "react-router-dom"
-import { Navbar, Hero, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas } from "./components"
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from './components';
+import Preview from './components/Preview';
 
 function App() {
-
-  BrowserRouter
+  BrowserRouter;
 
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
-        <Hero/>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        {/* <Experience/> */}
+        <Tech />
+        <Works />
+        {/* <Feedbacks/> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About/>
-      {/* <Experience/> */}
-      <Tech/>
-      <Works/>
-      {/* <Feedbacks/> */}
-      <div className="relative z-0">
-        <Contact/>
-        <StarsCanvas/>
-      </div>
-    </div>
+      <Routes>
+        <Route exact path="/preview" element={<Preview />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
